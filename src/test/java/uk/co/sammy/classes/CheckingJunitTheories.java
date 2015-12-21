a88684df6541ff3dd7e3e645f94525bcaab54f90
Samuel Ifere <sml.ifere@gmail.com>
2015-12-21 10:01:46 +0000

    Adds a theory test class

---
+++ src/test/java/uk/co/sammy/classes/CheckingJunitTheories.java
@@ -0,0 +1,33 @@
+package uk.co.sammy.classes;
+
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by smlif on 21/12/2015.
+ */
+@RunWith(Theories.class)
+public class CheckingJunitTheories {
+
+    @DataPoint
+    public static String jack = "Jack";
+
+    @DataPoint
+    public static String mike = "Michael";
+
+    @DataPoints
+    public static char[] letters = new char[]{'A', 'B', 'C'};
+
+    @Theory
+    public void sanity(String firstName, String lastName){
+        System.out.println("Sanity check " + firstName + " " + lastName);
+    }
+
+    @Theory
+    public void build(char c, char d){
+        System.out.println(c + " " + d);
+    }
+}
